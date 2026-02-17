348e29113ea6c3661b2effc2859c6932d17f6552
unknown <rahuldeewach1@gmail.com>
2026-02-17 13:28:22 -0800

    second cmmit

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-917c@3eztq#(+0sci_v05la(dbrrq^=8-hz)r=o9=@liuz()6k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
